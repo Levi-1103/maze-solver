38c46d8262257999cc540f3945d7a05c60c425da
Levi-1103 <58707841+Levi-1103@users.noreply.github.com>
2024-10-16 00:44:48 +0100

    Added Point and Line class + function to draw a line

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 
-from graphics import Window
+from graphics import Point, Window, Line
 
 def main():
     win = Window(800, 600)
+    line = Line(Point(50, 50), Point(400, 400))
+    win.draw_line(line,"black")
     win.wait_for_close()
-
-
-
+  
 
 if __name__ == "__main__":
     main()
